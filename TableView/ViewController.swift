2b3ac94b3f3044f7545c2b7f136e84833d1a1a19
abdulmoizahmer <abdulmoizahmer1996@gmail.om>
2019-02-05 12:40:48 +0500

    Initial Commit

---
+++ TableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableView
+//
+//  Created by Abdulmoiz Ahmer on 05/02/2019.
+//  Copyright © 2019 Abdulmoiz Ahmer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
