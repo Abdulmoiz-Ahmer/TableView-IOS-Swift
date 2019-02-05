fb69968cc0f51d4ba25e489bf15755d2944b1fc8
abdulmoizahmer <abdulmoizahmer1996@gmail.om>
2019-02-05 15:03:54 +0500

    Project Working in Xcode 10 with Swift 4

--- TableView/ViewController.swift
+++ TableView/ViewController.swift
@@ -8,13 +8,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDelegate, UITableViewDataSource {
 
+    let list = ["Albus Severus Potter",
+                "Dobby the House Elf",
+                "Draco Malfoy",
+                "Ginny Weasley",
+                "Harry Potter",
+                "Hermione Granger",
+                "Lily Evans Potter",
+                "Lord Voldemort",
+                "Lucius Malfoy",
+                "Luna Lovegood",
+                "Professor Albus Dumbledore",
+                "Professor Severus Snape",
+                "Ron Weasley",
+                "Rubeus Hagrid",
+                "Sirius Black"
+              ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return list.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: "cell")
+        cell.textLabel?.text = list[indexPath.row]
+        return cell
+        
+    }
 
-
+    
 }
 
